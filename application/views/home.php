1d99fcfde542c163e0996b44915cabce17d15246
PeaceS <p.saravudecha@gmail.com>
2014-08-03 19:55:31 +0700

    Start

---
+++ application/views/home.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Wednesday Child</title>
+        <link type="text/css" rel="stylesheet/less" href="<?php echo base_url(); ?>assets/style/home.less" />
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/script/lib/less-1.5.0.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/script/home.js"></script>
+</head>
+<body>
+	<div id="home_banner">
+		<label></label>
+                <div></div>
+		<ul>
+			<li>collection</li>
+			<li>shop</li>
+			<li>press</li>
+			<li>contacts</li>
+		</ul>
+	</div>
+</body>
+</html>
