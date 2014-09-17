3b877c6340fb7814b0f421c4f338f83b6c2e32b4
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-09-17 09:47:00 +0700

    Fix url wrong on a tag

--- application/views/list_menu.php
+++ application/views/list_menu.php
@@ -1,8 +1,8 @@
         <ul id="list_menu">
-            <li><a href="collection">collection</a></li>
-            <li><a href="shop">shop</a></li>
-            <li><a href="press">press</a></li>
-            <li><a href="contacts">contacts</a></li>
+            <li><a href="/collection">collection</a></li>
+            <li><a href="/shop">shop</a></li>
+            <li><a href="/press">press</a></li>
+            <li><a href="/contacts">contacts</a></li>
         </ul>
     </div>
 </body>
