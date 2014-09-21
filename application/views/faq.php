87a483555ebdd0dd0784186f74c975f8e4248d94
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-09-22 06:58:49 +0700

    Fix home page layout

---
+++ application/views/faq.php
@@ -0,0 +1,6 @@
+        <div id="policy" class="white_opacity">
+            <label>policy</label>
+            <div id="scroll">
+                <div><?php echo $policy; ?></div>
+            </div>
+        </div>
