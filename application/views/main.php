1c1ca7a70e596270e44fd16f4ef69e0459fae32f
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-09-22 23:13:44 +0700

    Fix some bugs on main, policy

--- application/views/main.php
+++ application/views/main.php
@@ -3,7 +3,7 @@
         <label id="logo"></label>
         <div id="top_menu">
             <span id="menu_purchaseConfirm">purchase confirm</span>
-            <span id="menu_faq">faq</span>
+            <a href="/faq"><span id="menu_faq">faq</span></a>
             <a href="/policy"><span id="menu_policy">policy</span></a>
             <span id="menu_mybag">mybag(<amount></amount>)</span>
         </div>
