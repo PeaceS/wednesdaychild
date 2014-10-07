148c4226d91e7c2be55b4a32e9aaf43ed5518e22
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-10-07 21:45:47 +0700

    Fix on buy_menu, clickable when have pass

--- application/views/buy_menu.php
+++ application/views/buy_menu.php
@@ -1,7 +1,13 @@
             <div id="payment_menu">
-                <a <?php if ($buy == 1) { ?>class="selected"<?php } ?> href="/buy/1">BAGS</a>/
-                <a <?php if ($buy == 2) { ?>class="selected"<?php } ?> href="/buy/2">Shipping Address</a>/
-                <a <?php if ($buy == 3) { ?>class="selected"<?php } ?> href="/buy/3">Summary</a>/
-                <a <?php if ($buy == 4) { ?>class="selected"<?php } ?> href="/buy/4">Payment Method</a>/
+                <a <?php if ($buy == 1) { ?>class="selected"<?php } ?> href="/buy/1">BAGS </a>/
+                <?php if ($buy >= 2 || isset($bag)) { ?><a <?php if ($buy == 2) { ?>class="selected"<?php } ?> href="/buy/2"><?php } ?>
+                    Shipping Address
+                <?php if ($buy >= 2 || isset($bag)) { ?></a><?php } ?>/
+                <?php if ($buy >= 3 || isset($shippingAddress)) { ?><a <?php if ($buy == 3) { ?>class="selected"<?php } ?> href="/buy/3"><?php } ?>
+                    Summary
+                <?php if ($buy >= 3 || isset($shippingAddress)) { ?></a><?php } ?>/
+                <?php if ($buy == 4 || isset($shippingAddress)) { ?><a <?php if ($buy == 4) { ?>class="selected"<?php } ?> href="/buy/4"><?php } ?>
+                    Payment Method
+                <?php if ($buy == 4 || isset($shippingAddress)) { ?></a><?php } ?>/
             </div>
         </div>
