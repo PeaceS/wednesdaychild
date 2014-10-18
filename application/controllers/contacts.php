355234149b87765c7040d90b3f4d705d89074943
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-10-18 07:40:53 +0700

    Done UI on contact page

--- application/controllers/contacts.php
+++ application/controllers/contacts.php
@@ -1,13 +1,14 @@
 <?php if ( ! defined('BASEPATH')){ exit('No direct script access allowed'); }
 
-class Contact extends CI_Controller {
+class Contacts extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
 	$this->load->model('get_config');
     }
     public function index()
-    {   
+    {
+        $this->load->helper('form');
         $included['contact'] = true;
         $data['contact'] = $this->get_config->get_contact();
         $data['itemCountInBag'] = $this->get_session->get_itemCountInBag();
