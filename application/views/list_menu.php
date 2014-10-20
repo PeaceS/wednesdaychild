ea0c2eebd8e72c6a5c913aa9336c8f278319b94b
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-10-20 07:08:31 +0700

    Add fb,ig to list_menu, still show every page now, need to talk which page to show, and where to show exactly

--- application/views/list_menu.php
+++ application/views/list_menu.php
@@ -4,5 +4,13 @@
             <li><a href="/press">press</a></li>
             <li><a href="/contacts">contacts</a></li>
         </ul>
+        <ul id="social">
+            <li><a href="https://www.facebook.com/wednesdaychildshop">
+                    <img src="<?php echo base_url(); ?>assets/image/icon_facebook.svg" title="facebook" />
+            </a></li>
+            <li><a href="https://www.instagram.com/wednesday_child">
+                    <img src="<?php echo base_url(); ?>assets/image/icon_instagram.svg" title="instagram" />
+            </a></li>
+        </ul>
     </div>
 </body>
