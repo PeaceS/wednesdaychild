2726f34dbd966b44d5be37e2e7b5f4b71244e137
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-10-29 12:54:49 +0700

    Move facebook and instagram icon to main, do not show on home page

--- application/views/main.php
+++ application/views/main.php
@@ -7,3 +7,11 @@
             <a href="/policy"><span id="menu_policy">policy</span></a>
             <a href="/buy/1"><span id="menu_mybag">mybag(<amount><?php echo $itemCountInBag; ?></amount>)</span></a>
         </div>
+        <ul id="social">
+            <li><a href="https://www.facebook.com/wednesdaychildshop">
+                    <img src="<?php echo base_url(); ?>assets/image/icon_facebook.svg" title="facebook" />
+                </a></li>
+            <li><a href="https://www.instagram.com/wednesday_child">
+                    <img src="<?php echo base_url(); ?>assets/image/icon_instagram.svg" title="instagram" />
+                </a></li>
+        </ul>
