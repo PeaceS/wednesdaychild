ae881260723607ff4f9a7d5e71b850798f310ecf
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2015-01-03 14:02:42 +0700

    fix popup

--- assets/script/product.js
+++ assets/script/product.js
@@ -82,7 +82,7 @@ function event_change_product(product){
 function event_buy_product(product, amount){
     var data = {"product" : product, "qty" : amount};
     
-    $.post("/buy", data, function(result){
+    $.post("/wednesdaychild/buy", data, function(result){
         if (!isNaN(result)){
             show_popup("the item has added to cart");
             $("#menu_mybag amount").text(result);
