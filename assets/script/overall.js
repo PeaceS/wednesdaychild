d665372faa962cacea54b8518a98ebb66aa7369c
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2015-01-03 14:08:38 +0700

    fix popup, using fadeIn-Out instead, due to easier maintenace

--- assets/script/overall.js
+++ assets/script/overall.js
@@ -1,9 +1,7 @@
 
 function show_popup(wording){
-    $("#blur").removeClass();
-    $("#blur").addClass("show");
     $("#blur section span").text(wording);
-    setTimeout(function(){
-    	$("#blur").addClass("hide");
-    }, 3000);
+    $("#blur").fadeIn(3000, function(){
+    	$("#blur").fadeOut(2000);
+    });
 }
