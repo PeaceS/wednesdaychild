72e617ddab35edd3e378a2cc8671875b0c839e95
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2015-01-10 20:10:12 +0700

    fix shop page

--- application/views/main.php
+++ application/views/main.php
@@ -8,16 +8,20 @@
     <div id="main">
         <label id="logo"></label>
         <div id="top_menu">
-            <a href="/confirm"><span id="menu_purchaseConfirm">purchase confirm</span></a>
-            <a href="/faq"><span id="menu_faq">faq</span></a>
-            <a href="/policy"><span id="menu_policy">policy</span></a>
-            <a href="/buy/1"><span id="menu_mybag">mybag(<amount><?php echo $itemCountInBag; ?></amount>)</span></a>
+            <p>
+                <a href="/confirm"><span id="menu_purchaseConfirm">purchase confirm</span></a>
+            </p>
+            <p>
+                <a href="/faq"><span id="menu_faq">faq</span></a>
+                <a href="/policy"><span id="menu_policy">policy</span></a>
+                <a href="/buy/1"><span id="menu_mybag">mybag(<amount><?php echo $itemCountInBag; ?></amount>)</span></a>
+            </p>
         </div>
         <ul id="social">
-            <li><a href="https://www.facebook.com/wednesdaychildshop">
-                    <img src="<?php echo base_url(); ?>assets/image/icon_facebook.svg" title="facebook" />
-                </a></li>
             <li><a href="https://www.instagram.com/wednesday_child">
                     <img src="<?php echo base_url(); ?>assets/image/icon_instagram.svg" title="instagram" />
+                </a></li>
+            <li><a href="https://www.facebook.com/wednesdaychildshop">
+                    <img src="<?php echo base_url(); ?>assets/image/icon_facebook.svg" title="facebook" />
                 </a></li>
         </ul>
