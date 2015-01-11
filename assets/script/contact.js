678c0559e5852cacaefb6f17860468c391e9ef79
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2015-01-11 10:59:32 +0700

    fix contacts page

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 });
 
 function setup_default(){
-    default_set_fixWidthTextarea();
 }
 
 function setup_eventHandle(){
@@ -15,17 +14,6 @@ function setup_eventHandle(){
 
 // private function
 
-function default_set_fixWidthTextarea(){
-    var textarea = $("#contact #message textarea");
-    
-    textarea.css({
-        "height": $("#message").height()/1.5,
-        "min-width": textarea.innerWidth(),
-        "max-width": textarea.innerWidth(),
-        "min-height": $("#message").height()/1.5,
-        "max-height": $("#message").height()/1.5
-    });
-}
 function event_send_mail(){
     var data = {
         "name" : $("#message #name").val(),
