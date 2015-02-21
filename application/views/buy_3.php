8725085cde39d1028d768a445d5a90fe60ac68a2
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2015-02-21 17:15:11 +0700

    fix layout page buy_3

--- application/views/buy_3.php
+++ application/views/buy_3.php
@@ -1,15 +1,17 @@
         <div id="summary" class="white_opacity">
             <div id="scroll">
+                <table>
+                    <tr><td colspan="5">Purchased List</td></tr>
+                    <tr>
+                        <td width="175px">Item Name</td>
+                        <td width="50px">Size</td>
+                        <td width="50px">Color</td>
+                        <td width="75px">Quantity</td>
+                        <td width="100px">Price(s)</td>
+                    </tr>
+                </table>
                 <div id="sub_scroll">
-                    Purchased List
                     <table>
-                        <tr>
-                            <td width="40%">Item Name</td>
-                            <td width="12%">Size</td>
-                            <td width="13%">Color</td>
-                            <td width="17%">Quantity</td>
-                            <td width="18%">Price(s)</td>
-                        </tr>
                         <?php foreach ($bag as $item) { ?>
                         <tr class="item" product="<?php echo $item['product']; ?>">
                             <td class="name"><?php echo $item['name']; ?></td>
@@ -35,23 +37,24 @@
                         </tr>
                         <?php } ?>
                     </table>
+                    <hr>
                     <table id="price_and_shipping">
                         <tr>
-                            <td width="82%">Shipping Cost</td>
-                            <td width="18%"><?php echo number_format($shippingCost, 2, '.', ','); ?></td>
+                            <td width="360px">Shipping Cost</td>
+                            <td width="90px"><?php echo number_format($shippingCost, 2, '.', ','); ?></td>
                         </tr>
                         <tr>
                             <td>Total Price</td>
                             <td><?php echo number_format($totalPrice, 2, '.', ','); ?></td>
                         </tr>
                         <tr><td>Billing Address</td></tr>
-                        <tr><td><?php echo $shippingAddress['first'] . " " . $shippingAddress['last']; ?></td></tr>
-                        <tr>
+                        <tr class="address"><td><?php echo $shippingAddress['first'] . " " . $shippingAddress['last']; ?></td></tr>
+                        <tr class="address">
                             <td>
                                 <?php echo $shippingAddress['address'] . ", " . $shippingAddress['city'] . " " . $shippingAddress['zip'] . ", " . $shippingAddress['country']; ?>
                             </td>
                         </tr>
-                        <tr>
+                        <tr class="address">
                             <td>
                                 <?php echo $shippingAddress['phone'] . " | " . $shippingAddress['email']; ?>
                             </td>
@@ -67,3 +70,4 @@
                 <a href="/buy/2"><label class="back">back</label></a>
                 <a href="/buy/4"><label class="next">confirm</label></a>
             </div>
+            
