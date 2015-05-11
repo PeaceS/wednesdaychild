509db8e83d54d152eed78e23dc3575959c4b93de
PeaceS <p.saravudecha@gmail.com>
2015-05-11 22:13:31 +0700

    change list_menu

--- application/views/list_menu.php
+++ application/views/list_menu.php
@@ -1,7 +1,7 @@
         <ul id="list_menu">
             <li><a href="/collection">collection</a></li>
             <li><a href="/shop">shop</a></li>
-            <li><a href="/press">press</a></li>
+            <li><a href="/faq">faq</a></li>
             <li><a href="/contacts">contacts</a></li>
         </ul>
     </div>
